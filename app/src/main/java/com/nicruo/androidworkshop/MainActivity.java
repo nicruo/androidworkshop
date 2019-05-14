3694aa33daa15e594ae45ef160684ef875065bb5
nicruo <nicruo@gmail.com>
2019-05-14 21:34:05 +0100

    Added simple webservice request and json parse;

--- app/src/main/java/com/nicruo/androidworkshop/MainActivity.java
+++ app/src/main/java/com/nicruo/androidworkshop/MainActivity.java
@@ -2,11 +2,26 @@ package com.nicruo.androidworkshop;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +41,46 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(nameEditText.getText().toString().equals("nome")) {
-                    loginButton.setText(R.string.loaded);
-                    Intent intent = new Intent(context, HomeActivity.class);
-                    intent.putExtra("name", "nome");
-                    startActivity(intent);
+
+                    new AsyncTask<Void, Void, String>() {
+
+                        @Override
+                        protected String doInBackground(Void... voids) {
+                            try {
+                                URL pokeapiEndpoint = null;
+                                pokeapiEndpoint = new URL("https://pokeapi.co/api/v2/berry/2/");
+                                HttpsURLConnection myConnection = (HttpsURLConnection) pokeapiEndpoint.openConnection();
+
+                                int responseCode = myConnection.getResponseCode();
+
+                                InputStream responseBody = myConnection.getInputStream();
+                                InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+
+                                JsonObject root = new JsonParser().parse(responseBodyReader).getAsJsonObject();
+
+
+                                return root.get("flavors").getAsJsonArray().get(0).getAsJsonObject().get("flavor").getAsJsonObject().get("name").getAsString();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
+
+
+                        @Override
+                        protected void onPostExecute(String value) {
+                            super.onPostExecute(value);
+                            loginButton.setText(value);
+                            /*Intent intent = new Intent(context, HomeActivity.class);
+                            intent.putExtra("name", "nome");
+                            startActivity(intent);*/
+                        }
+                    }.execute();
+
+
+
+
+
                 }
             }
         });
